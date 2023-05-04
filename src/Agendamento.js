bb0a735fd9b4651d697ec6c1365b3844e367eeb7
YagoArruda <yago4444@gmail.com>
2023-05-04 11:23:47 -0300

    Alterações e Js da tela de agendamento

---
+++ src/Agendamento.js
@@ -0,0 +1,17 @@
+function limpeza(button) {
+    var Title = document.getElementById("limpTitle");
+    var Desc = document.getElementById("limpDesc");
+
+    if (button == 1) {
+        Title.textContent = "Limpeza Rapida";
+        Desc.textContent ="Limpeza nas maquinas com enxague automatico, pinheiro aromatico";
+    }
+    else if(button ==2){
+        Title.textContent = "Limpeza Normal";
+        Desc.textContent ="Limpeza nas maquinas com enxague automatico, Pinheiro aromatico, Aspiração completa, Hidratação do banco e Pretinho no pneu";
+    }
+    else{
+        Title.textContent = "Limpeza Completa";
+        Desc.textContent ="Limpeza nas maquinas com enxague automatico, Pinheiro aromatico, Aspiração completa e Pretinho no pneu";
+    }
+}
