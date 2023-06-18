bb671a8f5b01709c0692b2d187fcec0245dc7324
YagoArruda <yago4444@gmail.com>
2023-06-18 18:42:20 -0300

    Correção do commit anterior

--- src/Scripts/Usuarios.js
+++ src/Scripts/Usuarios.js
@@ -1,5 +1,26 @@
 //Usuario
 
+//Mascara para preenchimento do numero
+function mascaraTelefone() {
+    elemento = document.getElementById("InputNumero");
+    var conteudo = elemento.value;
+    var text;
+
+    if (conteudo.length == 2) {
+        text = "(" + conteudo.charAt(0) + conteudo.charAt(1) + ")" + " ";
+        elemento.value = text;
+    }
+    if (conteudo.length == 6) {
+        text = "(" + conteudo.charAt(1) + conteudo.charAt(2) + ")" + " " + conteudo.charAt(5) + " ";
+        elemento.value = text;
+    }
+    if (conteudo.length == 11) {
+        text = "(" + conteudo.charAt(1) + conteudo.charAt(2) + ")" + " " + conteudo.charAt(5) + " " + conteudo.charAt(7) + conteudo.charAt(8) + conteudo.charAt(9) + conteudo.charAt(10) + "-";
+        elemento.value = text;
+    }
+
+}
+
 //Le os cadastros salvos localmente
 function lerCadastrosSalvos() {
     var usuarios = localStorage.getItem('usuarios')
@@ -13,7 +34,7 @@ function lerCadastrosSalvos() {
     return usuarios
 }
 
-var imagem;
+var imagem = "nc";
 
 //Salva od dados do cadastro
 function salvarCadastro() {
@@ -40,50 +61,80 @@ function salvarCadastro() {
         }
         else {
             var usuarios = lerCadastrosSalvos()
-            usuarios.cadastrados = usuarios.cadastros.length;
+            let jaUsado = false;
 
-            const novoUsuario = {
-                id: usuarios.cadastrados--,
-                imagem: imagem,
-                usuario: nomeUsuario,
-                nome: nomeReal,
-                email: email,
-                senha: senha,
-                telefone: telefone,
-                termos: termos,
-                manterConectado: "nc",
-                carro: [{
-                    categoria: "nc",
-                    modelo: "nc",
-                    placa: "nc",
-                    cor: "nc",
-                }]
+            for (let i = 0; i < usuarios.cadastros.length; i++) {
 
-            };
+                if (usuarios.cadastros[i].email == email) {
+                    const toastLiveExample = document.getElementById('liveToast')
+                    document.getElementById('toastMensage').textContent = `"${email}" já foi cadastrado`
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    toastBootstrap.show()
+                    jaUsado = true;
+                    break;
+                }
 
-            usuarios.cadastros.push(novoUsuario)
-            usuarios.cadastrados = usuarios.cadastros.length;
+                if (usuarios.cadastros[i].usuario == nomeUsuario) {
+                    const toastLiveExample = document.getElementById('liveToast')
+                    document.getElementById('toastMensage').textContent = `"${nomeUsuario}" não está disponivel`
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    toastBootstrap.show()
+                    jaUsado = true;
+                    break;
+                }
 
-            localStorage.setItem('usuarios', JSON.stringify(usuarios));
-            window.location.href = "CadastroCarro.html"
+            }
+
+            if (jaUsado == false) {
+
+                usuarios.cadastrados = usuarios.cadastros.length;
+
+                const novoUsuario = {
+                    id: usuarios.cadastrados--,
+                    imagem: imagem,
+                    usuario: nomeUsuario,
+                    nome: nomeReal,
+                    email: email,
+                    senha: senha,
+                    telefone: telefone,
+                    termos: termos,
+                    manterConectado: "nc",
+                    carro: [{
+                        categoria: "nc",
+                        modelo: "nc",
+                        placa: "nc",
+                        cor: "nc",
+                    }]
+
+                };
+
+                usuarios.cadastros.push(novoUsuario)
+                usuarios.cadastrados = usuarios.cadastros.length;
+
+                usuarios.usuarioAtual = novoUsuario.id;
+                localStorage.setItem('usuarios', JSON.stringify(usuarios));
+                window.location.href = "CadastroCarro.html"
+
+            }
+
         }
     }
 
 }
 
 //acionado quando na pagina de cadastro de usuario
-function eventPagDeCDUsuario(){
+function eventPagDeCDUsuario() {
     //Detecta alterações no input da imagem
     const inputElement = document.getElementById('inputImagem');
-    
+
     inputElement.addEventListener('change', (event) => {
         const file = event.target.files[0];
-    
+
         convertImageToBase64(file, (base64Image) => {
-           // console.log(base64Image);
+            // console.log(base64Image);
             imagem = base64Image
             document.getElementById("imgPerfil").src = `data:image/png;base64,${imagem}`
-            document.getElementById("imgPerfil").style = `max-width: 170px;`
+            document.getElementById("imgPerfil").style = `width: 170px; height: 170px;`
         });
     });
 }
@@ -115,7 +166,7 @@ function convertImageToBase64(file, callback) {
     reader.readAsDataURL(file);
 }
 
-function inserirDadosBase(usuarios){
+function inserirDadosBase(usuarios) {
     //var usuarios = localStorage.getItem('usuarios')
 
     const novoUsuario = {
@@ -124,7 +175,7 @@ function inserirDadosBase(usuarios){
         cadastros: [
             {
                 id: 0,
-                imagem: "img/lavaDriveLogo.png",
+                imagem: "nc",
                 usuario: "Administrador",
                 nome: "LavaDriveADM",
                 email: "lavadrive@gmail.com",
@@ -148,39 +199,165 @@ function inserirDadosBase(usuarios){
     return usuarios;
 }
 
+
+
+
 //Veiculo
 
-//Detecta a conta atual
-function SalvarCarro(){
-   let usuarios = lerCadastrosSalvos()
+//Salva os dados do carro em cadastro
+function SalvarCarro() {
+    let usuarios = lerCadastrosSalvos()
 
-   let corInput = document.getElementById("inputcor").value
-   let placaInput = document.getElementById("inputplaca").value
-   let modeloInput = document.getElementById("inputmodelo").value
-   let categInput = document.getElementById("inputcategoria").value
+    let corInput = document.getElementById("inputcor").value
+    let placaInput = document.getElementById("inputplaca").value
+    let modeloInput = document.getElementById("inputmodelo").value
+    let categInput = document.getElementById("inputcategoria").value
 
-   if(modeloInput == " " || corInput == " " || placaInput == " " || categInput == "nc"){
-    const toastLiveExample = document.getElementById('liveToast')
+    if (modeloInput == " " || corInput == " " || placaInput == " " || categInput == "nc") {
+        const toastLiveExample = document.getElementById('liveToast')
         document.getElementById('toastMensage').textContent = "Para cadastrar um veiculo preencha todos os campos"
         const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
         toastBootstrap.show()
-   }
-   else{
-       for(let i = 0; i < usuarios.cadastros.length;i++){
-        if(usuarios.cadastros[i].id = usuarioAtual){
-            
-            let carroEmCadastro = usuarios.cadastros[i].carro.length
-
-            let carroInf = usuarios.cadastros[i].carro[carroEmCadastro]
-    
-            carroInf.categoria = categInput
-            carroInf.modelo = modeloInput
-            carroInf.placa = placaInput
-            carroInf.cor = corInput
+    }
+    else {
+        const DadosCarro = {
+            categoria: categInput,
+            modelo: modeloInput,
+            placa: placaInput,
+            cor: corInput,
         }
-       }
-   }
+
+        if (usuarios.cadastros[usuarios.usuarioAtual].carro.length == 1) {
+            usuarios.cadastros[usuarios.usuarioAtual].carro[0] = DadosCarro;
+        }
+        else {
+            usuarios.cadastros[usuarios.usuarioAtual].carro.push(DadosCarro);
+        }
+
+
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        window.location.href = "index.html"
+    }
 
 }
 
 
+
+
+//Login
+function logar() {
+    let usuarios = lerCadastrosSalvos()
+
+    let user = document.getElementById("inputUserLogin").value
+    let senha = document.getElementById("inputSenhaLogin").value
+    let manterConectado = document.getElementById("inputManterConectado").checked
+
+    for (let i = 0; i < usuarios.cadastros.length; i++) {
+        if (usuarios.cadastros[i].email == user || usuarios.cadastros[i].usuario == user) {
+            if (usuarios.cadastros[i].senha == senha) {
+                usuarios.cadastros[i].manterConectado = manterConectado;
+                usuarios.usuarioAtual = usuarios.cadastros[i].id;
+                localStorage.setItem('usuarios', JSON.stringify(usuarios));
+                window.location.href = "index.html"
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+//Perfil
+function dadosPerfil() {
+    let usuarios = lerCadastrosSalvos()
+
+    document.getElementById("perfilInputNome").value = usuarios.cadastros[usuarios.usuarioAtual].nome;
+    document.getElementById("perfilInputUsuario").value = usuarios.cadastros[usuarios.usuarioAtual].usuario;
+    document.getElementById("perfilInputEmail").value = usuarios.cadastros[usuarios.usuarioAtual].email;
+    document.getElementById("perfilInputTelefone").value = usuarios.cadastros[usuarios.usuarioAtual].telefone;
+
+    if (usuarios.cadastros[usuarios.usuarioAtual].imagem != "nc") {
+        document.getElementById("perfilImage").src = `${usuarios.cadastros[usuarios.usuarioAtual].imagem}`
+        document.getElementById("perfilImage").style = `width: 170px; height: 170px;`
+    }
+}
+
+
+//detecta as alterações nas informações //desativado temporariamente
+function alteracoes() {
+    let usuarios = lerCadastrosSalvos()
+
+    let nomeCad = usuarios.cadastros[usuarios.usuarioAtual].nome;
+    let usuarioCad = usuarios.cadastros[usuarios.usuarioAtual].usuario;
+    let emailCad = usuarios.cadastros[usuarios.usuarioAtual].email;
+    let telefoneCad = usuarios.cadastros[usuarios.usuarioAtual].telefone;
+
+    let imagemCad = usuarios.cadastros[usuarios.usuarioAtual].imagem;
+
+    let imagem = document.getElementById("perfilImage").src;
+
+    let nome = document.getElementById("perfilInputNome").value;
+    let usuario = document.getElementById("perfilInputUsuario").value;
+    let email = document.getElementById("perfilInputEmail").value;
+    let telefone = document.getElementById("perfilInputTelefone").value;
+
+    let botao = document.getElementById("salvar");
+
+    if (nome != nomeCad || usuario != usuarioCad || email != emailCad || telefone != telefoneCad || imagem != imagemCad) {
+        botao.disabled = false;
+    }
+    else {
+        botao.disabled = true;
+    }
+
+}
+
+//Salva as alterações feitas pelo o usuario
+function salvarAlteracoes() {
+    let usuarios = lerCadastrosSalvos()
+
+    let imagem = document.getElementById("perfilImage").src;
+
+    let nome = document.getElementById("perfilInputNome").value;
+    let usuario = document.getElementById("perfilInputUsuario").value;
+    let email = document.getElementById("perfilInputEmail").value;
+    let telefone = document.getElementById("perfilInputTelefone").value;
+
+    usuarios.cadastros[usuarios.usuarioAtual].nome = nome;
+    usuarios.cadastros[usuarios.usuarioAtual].usuario = usuario;
+    usuarios.cadastros[usuarios.usuarioAtual].email = email;
+    usuarios.cadastros[usuarios.usuarioAtual].telefone = telefone;
+
+    usuarios.cadastros[usuarios.usuarioAtual].imagem = imagem;
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    const toastLiveExample = document.getElementById('liveToast')
+    document.getElementById('toastMensage').textContent = "Alterações salvas com sucesso."
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastBootstrap.show()
+}
+
+//Acionado quando na pagina de perfil
+function imagemAlteracao() {
+    //Detecta alterações no input da imagem
+    const inputElement = document.getElementById('inputImagem');
+
+    inputElement.addEventListener('change', (event) => {
+        const file = event.target.files[0];
+
+        convertImageToBase64(file, (base64Image) => {
+            // console.log(base64Image);
+            imagem = base64Image
+            document.getElementById("perfilImage").src = `data:image/png;base64,${imagem}`
+            document.getElementById("perfilImage").style = `width: 170px; height: 170px;`
+        });
+    });
+}
+
+//Inicialização da pagina de perfil
+function initPerfil() {
+    dadosPerfil();
+    imagemAlteracao();
+}
