1d4c0f69e05807d0a3f8f4bb1fc532ba4dfce660
Daniel Vitor <danielsalviano879@gmail.com>
2023-06-27 16:35:28 -0300

    Enviando as modificações na parte da tela de administrador

--- src/Scripts/Administrador.js
+++ src/Scripts/Administrador.js
@@ -30,55 +30,53 @@ $(document).ready(function () {
     $toggleButtons.empty();
   });
 
-function Mostrar() {
-  fetch("https://api-avaliacao.vercel.app/agendamentos")
-    .then(res => res.json())
-    .then(data => {
-
-      let usuarios = lerCadastrosSalvos();
-
-      let tela = document.getElementById('tela');
-      let str = '';
-
-      let categorias = ["Hatch", "Sedan", "SUV", "Picape/Caminhonete", "Motocicleta", "Outro"];
-
-      str += `<div class="container">`;
-      str += `<div class="row">`;
-      for (let w = 0; w < data.length; w++) {
-        for (let k = 0; k < categorias.length; k++) {
-          str += `<div class="col-lg-2 col-md-4 col-sm-6 col-12">`;
-          str += `<h4 class="text-center">${categorias[k]}</h4>`;
-          for (let j = 0; j < usuarios.cadastros.length; j++) {
-            for (let i = 0; i < usuarios.cadastros[j].carro.length; i++) {
-              if (categorias[k] == usuarios.cadastros[j].carro[i].categoria) {
-                str +=
-                  `
-                        <div class="card mb-3" style="max-width: 16rem;">
-                          <div class="card-body">
-                            <p class="card-text">Modelo: ${usuarios.cadastros[j].carro[i].modelo}</p>
-                            <p class="card-text">Placa: ${usuarios.cadastros[j].carro[i].placa}</p>
-                            <p class="card-text">Cor: ${usuarios.cadastros[j].carro[i].cor}</p>
-                            <p class="card-text">Dia marcado: </p>
-                            <p class="card-text">Hora marcada: </p>
-                            <button class="btn btn-primary">Editar</button>
-                          </div>
-                        </div>`;
+  function Mostrar(){
+    fetch("https://api-avaliacao.vercel.app/agendamentos")
+                .then(res=>res.json())
+                .then(data=>{
+                 
+                  let usuarios = lerCadastrosSalvos();
+    
+                  let tela = document.getElementById('tela');
+                  let str = '';
+                
+                  let categorias = ["Hatch", "Sedan", "SUV", "Picape/Caminhonete", "Motocicleta", "Outro"];
+                
+                  str += `<div class="container">`;
+                  str += `<div class="row">`;
+                 
+                  for (let k = 0; k < categorias.length; k++) {
+                    str += `<div class="col-lg-2 col-md-4 col-sm-6 col-12">`;
+                    
+                    for (let j = 0; j < usuarios.cadastros.length; j++) {
+                      for (let i = 0; i < usuarios.cadastros[j].carro.length; i++) {
+                        if (categorias[k] == usuarios.cadastros[j].carro[i].categoria) {
+                          str +=
+                            `
+                            <div class="card mb-3" style="max-width: 16rem;">
+                              <div class="card-body">
+                              <p class="card-text">Modelo: ${usuarios.cadastros[j].carro[i].categoria}</p>
+                                <p class="card-text">Modelo: ${usuarios.cadastros[j].carro[i].modelo}</p>
+                                <p class="card-text">Placa: ${usuarios.cadastros[j].carro[i].placa}</p>
+                                <p class="card-text">Cor: ${usuarios.cadastros[j].carro[i].cor}</p>
+                                <p class="card-text">Dia marcado: </p>
+                                <p class="card-text">Hora marcada: </p>
+                                <button class="btn btn-primary">Editar</button>
+                              </div>
+                            </div>`;
+                        }
+                      }
+                    }
+                    str += `</div>`;
+                  }
+                
+                  str += `</div>`;
+                  str += `</div>`;
+                
+                  tela.innerHTML = str;
               }
-            }
-          }
-          str += `</div>`;
-        }
-      }
-      str += `</div>`;
-      str += `</div>`;
-
-      tela.innerHTML = str;
-
-      
+              )
     }
-    )
-}
-
 fetch('https://api-avaliacao.vercel.app/avaliacao')
   .then(response => response.json())
   .then(data => {
